b441b65cbcfde860a1812de5f03e7c4233ef5feb
yash-pratap-create <robinhoodyash15@gmail.com>
2026-04-24 15:07:20 +0530

    Yash Added UC1

---
+++ App/src/main/QuantityMeasurementApp.java
@@ -0,0 +1,27 @@
+package com.apps.quantitymeasurement;
+
+public class QuantityMeasurementApp {
+
+    public static class Feet {
+        private final double value;
+
+        public Feet(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null) return false;
+            if (getClass() != obj.getClass()) return false;
+
+            Feet other = (Feet) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            return Double.hashCode(value);
+        }
+    }
+}
